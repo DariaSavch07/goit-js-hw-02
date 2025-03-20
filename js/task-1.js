49b867b9d7b87929d8b8ad9f22ee09057750a194
Daria Savchenko <myworkpl24@gmail.com>
2025-03-20 19:49:35 +0100

    fails add

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,38 @@
 
- function makeTransaction(quantity, pricePerDroid, customerCredits) {
-   
-    const totalPrice = quantity * pricePerDroid;
-    
-
-    if (totalPrice > customerCredits) {
-        return "Insufficient funds!";
-    }
-    
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+    return title.toLowerCase().split(' ').join('-');
 }
 
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+function makeArray(firstArray, secondArray, maxLength) {
+    const newArray = [...firstArray, ...secondArray];
+    return newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+
+function filterArray(numbers, value) {
+    let filteredNumbers = [];
+    for (let number of numbers) {
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
+    }
+    return filteredNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
 
